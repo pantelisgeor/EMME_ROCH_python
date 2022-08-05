7c2ec62c7ae0e3c1c78d7dc123c8d8722df5214e
pantelisgeor <pantelisgeor@hotmail.com>
2022-08-05 15:22:49 +0300

    added check for missing dates

--- src/__init__.py
+++ src/__init__.py
@@ -2,9 +2,10 @@
 # local imports
 # --------------------------------------------------------------------- #
 
-from .climate_temporal import *
-from .downloadCDS import *
-from .eurostat_data import *
-from .geometries import *
+from .climate_temporal import parse_name, weekly_cdo
+from .downloadCDS import downloadCDS, downloadMultipleCDS
+from .eurostat_data import weekToDate, weeklyEurostat
+from .geometries import readNuts, make_polygon, \
+    getNutsclim, getNutsClimAll
 
 # --------------------------------------------------------------------- #
--- src/climate_temporal.py
+++ src/climate_temporal.py
@@ -1,8 +1,10 @@
 import os
 from glob import glob
+from warnings import WarningMessage
 import pandas as pd
 import re
 import datetime
+import warnings
 
 
 # ------------------------------------------------------------------------------- # 
@@ -24,6 +26,49 @@ def parse_name(x):
                              "filename": [x]})
     except Exception as e:
         print(f"\nERROR: {x} failed to be parsed.\n")
+
+
+# ------------------------------------------------------------------------------- # 
+def checkYears(files):
+    """Check if the dataset is complete before joining them.
+    
+    params:
+        files: pandas dataframe with the datasets information
+
+    returns:
+        pandas dataframe within missing dates if there are any, None otherwise
+    """
+    
+    # Get the min and max year in the files dataframe
+    min_year, max_year = files.year.min(), files.year.max()
+
+    # Find the months that are missing from each year
+    for year in range(min_year, max_year+1): 
+        # List the months in the dataset
+        months = files[files.year == year].month.values
+        present_months = set(range( 
+            files[files.year == year].month.min(), 
+            files[files.year == year].month.max() + 1 ))
+        # compare the set of the two
+        if set(months) != present_months:
+            # Get the missing months
+            missing_months = list(present_months.difference(set(months)))
+            if len(missing_months) > 1:
+                df_temp = pd.DataFrame({'year': year, 'months_missing': missing_months})
+            elif len(missing_months) == 1:
+                df_temp = pd.DataFrame({'year': [year], 'months_missing': [missing_months]})
+            # Add it to a dataframe to return
+            try:
+                df_res = pd.concat([df_res, df_temp])
+            except NameError:
+                df_res = df_temp
+            del months, present_months, df_temp, year
+    # Return
+    try:
+        return df_res
+    except UnboundLocalError:
+        return None
+
 
 
 # ------------------------------------------------------------------------------- # 
@@ -51,8 +96,15 @@ def weekly_cdo(path_dat, name_prefix, path_out=None):
     files.sort_values(by=['year', 'month'], ascending=True, inplace=True)
     files.reset_index(drop=True, inplace=True)
 
+    # Check if the datasets are complete (if there are missing dates between start and end)
+    df_data_complete = checkYears(files)
+    if df_data_complete is not None:
+        warnings.warn("\n        WARNING: There are missing dates in the datasets\n \
+            ----------- SEE BELOW MISSING DATES -----------\n")
+        print(df_data_complete)
+        print("\n         ------------------------------------------------")
+
     # Combine the data
-    cdo_ = cdo.Cdo()
     files_to_join = files[files.temp_res == 'hourly'].filename.values
     files_to_join = [f"{path_dat}/{x}" for x in files_to_join]
 
